ddb882171a5a13a6dd526cdf47e88e526a170ded
TomKugelman <52869328+TomKugelman@users.noreply.github.com>
2020-12-03 14:28:24 -0500

    Added Calculations

--- src/main/calculator.py
+++ src/main/calculator.py
@@ -1,13 +1,18 @@
 # Created by Leon Hunter at 9:54 AM 10/23/2020
+import math
+
+
 class Calculator(object):
     def add(self, a, b):
-        return None # TODO - Implement solution
+        return a + b
 
     def subtract(self, a, b):
-        return None # TODO - Implement solution
+        return a - b
 
     def multiply(self, a, b):
-        return None # TODO - Implement solution
+        return a * b
 
     def divide(self, a, b):
-        return None # TODO - Implement solution
+        if a <= 0 or b <= 0:
+            return "ZeroDivisionError"
+        return a / b
